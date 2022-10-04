1d6246ad2a844d98102501e8e981d8e091980be9
morikawa <c0b2115104@edu.teu.ac.jp>
2022-10-04 17:07:36 +0900

    途中経過

--- ex02/calc1.py
+++ ex02/calc1.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+count1 = 1
+
 def click_number(event):
+    global count1
     btn = event.widget
     num = btn["text"]
-    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
     entry.insert(tk.END, num)
+    count1 = 0
 
 def click_eq(event):
     eqn = entry.get()
@@ -13,27 +16,71 @@ def click_eq(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, res)
 
-root = tk.Tk()
-root.geometry("300x500")
+def click_k1(event):
+    global count1
+    btn = event.widget
+    num = btn["text"]
+    if count1 == 0:
+        if num == "÷":
+            entry.insert(tk.END, "/")
+            count1 += 1
+        elif num == "x":
+            entry.insert(tk.END, "*")
+            count1 += 1
+        elif num == "+":
+            entry.insert(tk.END, "+")
+            count1 += 1
+        elif num == "-":
+            entry.insert(tk.END, "-")
+            count1 += 1
+    else:
+        pass
 
-entry = tk.Entry(root, width=10, font=("", 40), justify="right")
-entry.grid(row=0, column=0, columnspan=3)
+def click_del(event):
+    entry.delete(0,tk.END)
+
+def click_Num(event):
+    pass
+
+root = tk.Tk()
+root.geometry("380x600")
+
+entry = tk.Entry(root, width=14, font=("", 40), justify="right")
+entry.grid(row=0, column=0, columnspan=6)
 
 r, c = 1, 0
 numbers = list(range(9, -1, -1))
-operators = ["+"]
+operators = ["+","-","x","÷"]
 
-for i, num in enumerate(numbers+operators, 1):
+for i, num in enumerate(numbers, 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
-    btn.grid(row=r, column=c)
+    btn.grid(row=r+1, column=c)
     c += 1
     if i%3 == 0:
         r += 1
         c = 0
+        if i == 9:
+            r = 4
+            c = 1
 
-btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+for n,s in enumerate(operators,1):
+    btn = tk.Button(root, text=f"{s}", font=("", 30), width=4, height=2)
+    if s == "+" or "-" or "x" or "÷":
+        btn.bind("<1>", click_k1)
+    
+    btn.grid(row=n, column=4)
+
+btn = tk.Button(root, text="=", font=("", 30), width=4, height=2)
 btn.bind("<1>", click_eq)
-btn.grid(row=r, column=c)
+btn.grid(row=5, column=2)
+
+btn = tk.Button(root, text="C", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_del)
+btn.grid(row=5, column=0)
+
+btn = tk.Button(root, text="Num", font=("", 30), width=4, height=2)
+btn.bind("<1>", click_del)
+btn.grid(row=1, column=0)
 
 root.mainloop()
