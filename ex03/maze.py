180f89dad6b7e2d8a193cfa57ce87d16caeb0ac4
morikawa <c0b2115104@edu.teu.ac.jp>
2022-10-18 15:30:59 +0900

    ３限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,16 +11,27 @@ def key_up(event):
     key = ""
 
 def main_proc(): #7
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
-
+        mx += 1
+    if maze_list[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -31,8 +42,12 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canv.pack() #2
 
+    maze_list = mm.make_maze(15, 9) #1が壁、0が床 #9
+    maze_show = mm.show_maze(canv, maze_list) #10
+
     tori = tk.PhotoImage(file="ex03/fig/3.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100, my*100
     canv.create_image(cx, cy, image=tori, tag="tori") #3
 
     key = "" #現在押されているキーを表す　#4
@@ -41,9 +56,5 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up) #6
 
     main_proc()
-
-    maze_list = mm.make_maze(15, 9) #1が壁、0が床 #9
-
-    maze_show = mm.show_maze(canv, maze_list) #10
 
     root.mainloop()
