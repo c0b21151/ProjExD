53fcc42ede6cca4409d16b268b14cf6249ea301a
morikawa <c0b2115104@edu.teu.ac.jp>
2022-10-18 14:18:38 +0900

    ３問

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,4 +7,9 @@ root.title("迷えるこうかとん") #1
 canv = tk.Canvas(root, width = 1500, height = 900, bg = "black")
 canv.pack() #2
 
+tori = tk.PhotoImage(file="ex03/fig/3.png")
+cx, cy = 300, 400
+canv.create_image(cx, cy, image=tori, tag="tori") #3
+
+
 root.mainloop()
