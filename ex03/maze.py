b3602ce7f9776bce9bdd528599a6f23434195ba2
morikawa <c0b2115104@edu.teu.ac.jp>
2022-10-18 17:05:36 +0900

    ４限課題

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 import maze_maker as mm #9
+import time
 
 def key_down(event):
     global key
@@ -13,6 +14,9 @@ def key_up(event):
 def main_proc(): #7
     global mx, my
     global cx, cy
+    global ismove
+    global goal
+    global gmsg
     if key == "Up":
         my -= 1
     if key == "Down":
@@ -21,8 +25,16 @@ def main_proc(): #7
         mx -= 1
     if key == "Right":
         mx += 1
+
     if maze_list[my][mx] == 0:
         cx, cy = mx*100+50, my*100+50
+        print(mx,my)
+        print(goal)
+        if key != "":
+            if ismove == False:
+                canv.create_image(cx, cy, image=move, tag="move")
+                canv.delete("start")
+                ismove = True
     else:
         if key == "Up":
             my += 1
@@ -32,10 +44,23 @@ def main_proc(): #7
             mx += 1
         if key == "Right":
             mx -= 1
-    canv.coords("tori", cx, cy)
-    root.after(100, main_proc)
-
+    
+    if mx == 1 and my == 1 and ismove == False:
+        canv.coords("start", cx, cy)
+        root.after(100, main_proc)
+    else:
+        canv.coords("move", cx, cy)
+        root.after(100, main_proc)
+        if mx == 13 and my == 7 and goal == False:
+            goal = True
+            if goal == True:
+                canv.create_image(cx, cy, image=goali, tag="goal")
+                canv.delete("move")
+                tkm.showinfo("goal","goal")
+            
+        
 if __name__ == "__main__":
+    
     root = tk.Tk()
     root.title("迷えるこうかとん") #1
 
@@ -45,10 +70,16 @@ if __name__ == "__main__":
     maze_list = mm.make_maze(15, 9) #1が壁、0が床 #9
     maze_show = mm.show_maze(canv, maze_list) #10
 
-    tori = tk.PhotoImage(file="ex03/fig/3.png")
+    ismove = False
+    goal = False
+    
+    sti = tk.PhotoImage(file="ex03/fig/4.png")
+    move = tk.PhotoImage(file="ex03/fig/3.png")
+    goali = tk.PhotoImage(file="ex03/fig/6.png")
     mx, my = 1, 1
-    cx, cy = mx*100, my*100
-    canv.create_image(cx, cy, image=tori, tag="tori") #3
+    cx, cy = mx*100+50, my*100+50 #3
+    canv.create_image(cx, cy, image=sti, tag="start")
+    
 
     key = "" #現在押されているキーを表す　#4
 
