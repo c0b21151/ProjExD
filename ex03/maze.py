d960f90afef7358efcf99a544ea93c3b1b48fde9
morikawa <c0b2115104@edu.teu.ac.jp>
2022-10-18 14:10:29 +0900

    ２問

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,9 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 root = tk.Tk()
-root.title("迷えるこうかとん")
+root.title("迷えるこうかとん") #1
+
+canv = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+canv.pack() #2
 
 root.mainloop()
